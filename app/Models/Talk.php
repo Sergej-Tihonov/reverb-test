e20ab0f02ee79f3d7907f3a57b9d873f5e1b85ce
Sergej Tihonov <sergej@visifo.com>
2024-11-23 23:50:14 +0100

    setup data structure with blueprint package

---
+++ app/Models/Talk.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Talk extends Model
+{
+    use HasFactory;
+
+    /**
+     * @var array
+     */
+    protected $guarded = ['id', 'created_at', 'updated_at'];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'id' => 'integer',
+        'speaker_id' => 'integer',
+    ];
+
+    public function speaker(): BelongsTo
+    {
+        return $this->belongsTo(Speaker::class);
+    }
+
+    public function conferences(): BelongsToMany
+    {
+        return $this->belongsToMany(Conference::class);
+    }
+}
