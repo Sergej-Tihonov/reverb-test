8cc99e6b62a772d255247ed708f56883e28bbd94
Sergej Tihonov <sergej@visifo.com>
2024-11-24 00:29:58 +0100

    setup resources

--- app/Models/Talk.php
+++ app/Models/Talk.php
@@ -7,6 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
+/**
+ * @mixin IdeHelperTalk
+ */
 class Talk extends Model
 {
     use HasFactory;
--- app/Models/Venue.php
+++ app/Models/Venue.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+/**
+ * @mixin IdeHelperVenue
+ */
 class Venue extends Model
 {
     use HasFactory;
